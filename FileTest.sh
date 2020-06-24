5c8825b996501bb02ecd23f5c5be86d61598943e
nguyenvinhson <66408202+nguyenvinhson@users.noreply.github.com>
2020-06-24 16:05:27 +0700

    create file

---
+++ FileTest.sh
@@ -0,0 +1,7 @@
+git worktree add [-f] [--detach] [--checkout] [--lock] [-b <new-branch>] <path> [<commit-ish>]
+git worktree list [--porcelain]
+git worktree lock [--reason <string>] <worktree>
+git worktree move <worktree> <new-path>
+git worktree prune [-n] [-v] [--expire <expire>]
+git worktree remove [-f] <worktree>
+git worktree unlock <worktree>
